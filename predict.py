9f630a885df8af4906c330f20e622215e1afc74b
Shivangid2904 <162672523+Shivangid2904@users.noreply.github.com>
2025-06-18 21:48:08 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -5,6 +5,5 @@ model = joblib.load("models/model.pkl")
 
 def predict(input_data):
     input_df = pd.DataFrame([input_data])
-    probability = model.predict_proba(input_df)[0][1]  # probability for class 1
-    prediction = int(probability >= 0.5)
-    return prediction, probability
+    prediction = model.predict(input_df)[0]
+    return prediction
